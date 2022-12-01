6c9344bc3c4d4d8c4212c80e966a87b9386988fb
GameGuyThrowaway <96356176+GameGuyThrowaway@users.noreply.github.com>
2022-11-30 17:55:03 -0800

    Update scriptBeta.lua

--- scriptBeta.lua
+++ scriptBeta.lua
@@ -13,8 +13,6 @@ if not RenderWindow then
     error("EXPLOIT NOT SUPPORTED - GET SYNAPSE V3")
 end
 
-syn.oth.unhook(mt.__namecall)
-
 local function cleanUpSpy()
     for _,v in _G.remoteSpyCallbackHooks do
         restorefunction(v)
